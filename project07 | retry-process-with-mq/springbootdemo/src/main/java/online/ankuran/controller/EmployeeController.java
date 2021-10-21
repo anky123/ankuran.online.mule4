689efa433bdfdf416a7352ae36b9115074551aca
Ankur <webmail.ajb@gmail.com>
2021-10-22 04:19:18 +0530

    Add code for retry and re-process POC. Update the postman scripts for all the mule4 projects

---
+++ project07 | retry-process-with-mq/springbootdemo/src/main/java/online/ankuran/controller/EmployeeController.java
@@ -0,0 +1,33 @@
+package online.ankuran.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import online.ankuran.model.Employee;
+
+@RestController
+public class EmployeeController {
+	
+	@RequestMapping("/getEmployees")
+    public List<Employee> getEmployees() 
+    {
+		List<Employee> employeesList = new ArrayList<Employee>();
+		employeesList.add(new Employee(1,"Ankur","Bhuyan","ankur.bhuyan@ankuran.online"));
+		employeesList.add(new Employee(2,"Ankur2","Bhuyan2","ankur.bhuyan2@ankuran.online"));
+		return employeesList;
+    }
+
+	@RequestMapping("/postEmployee")
+    public String postEmployee(@Valid @RequestBody Employee employee) 
+    {
+		String reponse = "Employee details validated stored in database succefully.";
+		System.out.println(reponse);
+		return reponse;
+    }
+}
